f08d41379bc916689e484c6d8101ecc52121d029
Sebastian Spaink <sspaink@influxdata.com>
2022-03-24 17:45:49 -0500

    fix: cpu sample config

--- plugins/inputs/cpu/cpu.go
+++ plugins/inputs/cpu/cpu.go
@@ -32,17 +32,7 @@ func NewCPUStats(ps system.PS) *CPUStats {
 }
 
 func (c *CPUStats) SampleConfig() string {
-	return `# Read metrics about cpu usage
-[[inputs.cpu]]
-  ## Whether to report per-cpu stats or not
-  percpu = true
-  ## Whether to report total system cpu stats or not
-  totalcpu = true
-  ## If true, collect raw CPU time metrics
-  collect_cpu_time = false
-  ## If true, compute and report the sum of all non-idle CPU states
-  report_active = false
-`
+	return `{{ .SampleConfig }}`
 }
 
 func (c *CPUStats) Gather(acc telegraf.Accumulator) error {
